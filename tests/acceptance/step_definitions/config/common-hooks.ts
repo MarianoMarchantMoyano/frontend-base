0e38a600bfc168ffbadabe883898d3c320fca489
Carlos Marin Diaz <carlosmarind@gmail.com>
2024-08-21 20:39:34 -0400

    generacion de pruebas de ui

--- tests/acceptance/step_definitions/config/common-hooks.ts
+++ tests/acceptance/step_definitions/config/common-hooks.ts
@@ -7,26 +7,26 @@ import { After, AfterAll, Before, BeforeAll, ITestCaseHookParameter, Status } fr
 
 let browser: ChromiumBrowser
 
-BeforeAll(async function() {
-    browser = await chromium.launch({ headless: false })
-  });
-  
-Before(async function(this: ICustomWorld) {
-  this.context = await browser.newContext({
+BeforeAll(async function () {
+	browser = await chromium.launch({ headless: false })
+});
+
+Before(async function (this: ICustomWorld) {
+	this.context = await browser.newContext({
 		acceptDownloads: true,
 		recordVideo: { dir: 'tests/reports/videos' },
 		viewport: { width: 1200, height: 800 }
 	});
-  this.page = await this.context.newPage();
+	this.page = await this.context.newPage();
 })
 
-After(async function(this: ICustomWorld) {
-  await this.page?.close();
-  await this.context?.close()
+After(async function (this: ICustomWorld) {
+	await this.page?.close();
+	await this.context?.close()
 });
-  
-AfterAll(async function() {
-  await browser.close()
+
+AfterAll(async function () {
+	await browser.close()
 });
 
 After(async function (this: ICustomWorld, { result }: ITestCaseHookParameter) {
@@ -46,7 +46,7 @@ After(async function (this: ICustomWorld, { result }: ITestCaseHookParameter) {
 		if (result.status !== Status.PASSED) {
 			const video = await this.page?.video()?.path()
 			const videoName = video?.split('/').pop()
-			var videoHtmlTag='<video src="videos/' + videoName + '" style="max-width: 100%; height: auto;" controls=""></video>';
+			var videoHtmlTag = '<video src="videos/' + videoName + '" style="max-width: 100%; height: auto;" controls=""></video>';
 			video && (await this.attach(videoHtmlTag, 'text/html'))
 
 		} else if (result.status === Status.PASSED) {
